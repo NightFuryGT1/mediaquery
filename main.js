660e42a91e10fcda080d9ce2d3c570df31db8481
NightFuryGT1 <armanditosigo@gmail.com>
2024-08-15 15:59:00 -0600

    Se agrega el diseño inicial

--- main.js
+++
@@ -1,6 +0,0 @@
-const menu = document.querySelector('.hamburger-menu');
-const nav = document.querySelector('.nav');
-
-menu.addEventListener('click', () => {
-    nav.classList.toggle('active');
-});
